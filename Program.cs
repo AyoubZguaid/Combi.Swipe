1ad67c723dd0454f72c0b19e936dfac469848395
amarianelli <a.marianelli@betclicgroup.com>
2025-09-25 14:28:37 +0200

    usecors

--- Program.cs
+++ Program.cs
@@ -14,7 +14,19 @@ builder.Services.AddSingleton(awsSettings);
 builder.Services.AddSingleton<IBedrockService, BedrockService>();
 builder.Services.AddScoped<ISelectionService, SelectionService>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 if (app.Environment.IsDevelopment())
 {
