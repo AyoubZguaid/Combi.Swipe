c5662c2aee2c24b71ae885a01e9820290c6806ea
e-dewilde <e.dewilde@betclicgroup.com>
2025-09-25 11:12:55 +0200

    add json

---
+++ Infrastructure/CustomersProfiles/CustomerAffinityModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Combi.Swipe.Infrastructure.CustomersProfiles
+{
+    public class CustomerAffinityModel
+    {
+        [JsonPropertyName("CUSTOMER_ID")]
+        public long CUSTOMER_ID { get; set; }
+
+        [JsonPropertyName("RISK_STYLE")]
+        public required string RISK_STYLE { get; set; } = string.Empty;
+
+        [JsonPropertyName("FAVORITE_TEAM")]
+        public required string FAVORITE_TEAM { get; set; } = string.Empty;
+
+        [JsonPropertyName("FAVORITE_MARKET_NAME")]
+        public required string FAVORITE_MARKET_NAME { get; set; } = string.Empty;
+    }
+}
---
+++ Infrastructure/Selections/SelectionModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Combi.Swipe.Infrastructure.Selections
+{
+    public class SelectionModel
+    {
+        [JsonPropertyName("SELECTION_ID")]
+        public string SelectionId { get; set; }
+
+        [JsonPropertyName("SELECTION_DESCRIPTION")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("MARKET_ID")]
+        public long MarketId { get; set; }
+
+        [JsonPropertyName("MARKET_NAME")]
+        public string MarketName { get; set; }
+
+        [JsonPropertyName("MATCH_ID")]
+        public long MatchId { get; set; }
+
+        [JsonPropertyName("ODDS")]
+        public decimal Odds { get; set; }
+
+        [JsonPropertyName("SPORT_ID")]
+        public string SportId { get; set; }
+    }
+}
